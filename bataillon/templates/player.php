32d15756c2ed4070320ea18b945f7a9fc133d27d
Dennis Becker <mail.dennisbecker@gmail.com>
2018-04-04 16:28:52 +0200

    adds Sith Raid Teams

--- bataillon/templates/player.php
+++ bataillon/templates/player.php
@@ -184,6 +184,7 @@
 
 	<div class="col-3">
 		<h5>Sith Raid</h5>
+		<h6>P1 / P4 - JTR (R2 or Visas Marr)</h6>
 		<table class="table">
 			<thead>
 			<tr>
@@ -195,6 +196,133 @@
 			</tr>
 			</thead>
 			<tbody>
+			<?php echo character('Rey (Jedi Training)', $characters); ?>
+			<?php echo character('BB-8', $characters); ?>
+			<?php echo character('R2-D2', $characters); ?>
+			<?php echo character('Visas Marr', $characters); ?>
+			<?php echo character('Rey (Scavenger)', $characters); ?>
+			<?php echo character('Resistance Trooper', $characters); ?>
+			</tbody>
+		</table>
+
+		<h6>P1 - Detonators</h6>
+		<table class="table">
+			<thead>
+			<tr>
+				<th>Character</th>
+				<th>Star</th>
+				<th>Level</th>
+				<th>Gear</th>
+				<th>Ready?</th>
+			</tr>
+			</thead>
+			<tbody>
+			<?php echo character('Grand Admiral Thrawn', $characters); ?>
+			<?php echo character('Hera Syndulla', $characters); ?>
+			<?php echo character('First Order Officer', $characters); ?>
+			<?php echo character('Ewok Elder', $characters); ?>
+			<?php echo character('Jawa Engineer', $characters); ?>
+			</tbody>
+		</table>
+
+		<h6>P2 - CLS</h6>
+		<table class="table">
+			<thead>
+			<tr>
+				<th>Character</th>
+				<th>Star</th>
+				<th>Level</th>
+				<th>Gear</th>
+				<th>Ready?</th>
+			</tr>
+			</thead>
+			<tbody>
+			<?php echo character('Commander Luke Skywalker', $characters); ?>
+			<?php echo character('Hermit Yoda', $characters); ?>
+			<?php echo character('Scarif Rebel Pathfinder', $characters); ?>
+			<?php echo character('Sabine Wren', $characters); ?>
+			<?php echo character('Ezra Bridger', $characters); ?>
+			</tbody>
+		</table>
+
+		<h6>P2 - Imperial Trooper</h6>
+		<table class="table">
+			<thead>
+			<tr>
+				<th>Character</th>
+				<th>Star</th>
+				<th>Level</th>
+				<th>Gear</th>
+				<th>Ready?</th>
+			</tr>
+			</thead>
+			<tbody>
+			<?php echo character('General Veers', $characters); ?>
+			<?php echo character('Grand Admiral Thrawn', $characters); ?>
+			<?php echo character('Death Trooper', $characters); ?>
+			<?php echo character('Shoretrooper', $characters); ?>
+			<?php echo character('Colonel Starck', $characters); ?>
+			</tbody>
+		</table>
+
+		<h6>P3 - CLS (Pao or Sabine)</h6>
+		<table class="table">
+			<thead>
+			<tr>
+				<th>Character</th>
+				<th>Star</th>
+				<th>Level</th>
+				<th>Gear</th>
+				<th>Ready?</th>
+			</tr>
+			</thead>
+			<tbody>
+			<?php echo character('Commander Luke Skywalker', $characters); ?>
+			<?php echo character('Han Solo', $characters); ?>
+			<?php echo character('Death Trooper', $characters); ?>
+			<?php echo character('Pao', $characters); ?>
+			<?php echo character('Sabine Wren', $characters); ?>
+			<?php echo character('Chirrut Îmwe', $characters); ?>
+			</tbody>
+		</table>
+
+		<h6>P4 - First Order</h6>
+		<table class="table">
+			<thead>
+			<tr>
+				<th>Character</th>
+				<th>Star</th>
+				<th>Level</th>
+				<th>Gear</th>
+				<th>Ready?</th>
+			</tr>
+			</thead>
+			<tbody>
+			<?php echo character('Kylo Ren (Unmasked)', $characters); ?>
+			<?php echo character('First Order Stormtrooper', $characters); ?>
+			<?php echo character('First Order Officer', $characters); ?>
+			<?php echo character('First Order Executioner', $characters); ?>
+			<?php echo character('First Order SF TIE Pilot', $characters); ?>
+			</tbody>
+		</table>
+
+		<h6>P4 - Nightsisters</h6>
+		<table class="table">
+			<thead>
+			<tr>
+				<th>Character</th>
+				<th>Star</th>
+				<th>Level</th>
+				<th>Gear</th>
+				<th>Ready?</th>
+			</tr>
+			</thead>
+			<tbody>
+			<?php echo character('Asajj Ventress', $characters); ?>
+			<?php echo character('Mother Talzin', $characters); ?>
+			<?php echo character('Old Daka', $characters); ?>
+			<?php echo character('Nightsister Acolyte', $characters); ?>
+			<?php echo character('Nightsister Zombie', $characters); ?>
 			</tbody>
 		</table>
 	</div>
