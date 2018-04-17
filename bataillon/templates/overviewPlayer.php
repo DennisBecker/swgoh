d1dc11065a9f2c9c73ca97c68e74bd7e6982be3f
Dennis Becker <mail.dennisbecker@gmail.com>
2018-04-17 09:26:41 +0200

    updates all data

--- bataillon/templates/overviewPlayer.php
+++ bataillon/templates/overviewPlayer.php
@@ -1,9 +1,12 @@
 <tr>
 	<td><?php echo $playerName; ?> - <?php echo $guild; ?></td>
-	<td><?php echo hasChar('Commander Luke Skywalker', $characters); ?></td>
 	<td><?php echo hasChar('Rey (Jedi Training)', $characters); ?></td>
 	<td><?php echo hasChar('Veteran Smuggler Han Solo', $characters); ?></td>
 	<td><?php echo hasChar('Veteran Smuggler Chewbacca', $characters); ?></td>
+	<td><?php echo hasChar('BB-8', $characters); ?></td>
+	<td><?php echo hasChar('R2-D2', $characters); ?></td>
+	<td><?php echo hasChar('Commander Luke Skywalker', $characters); ?></td>
+	<td><?php echo hasChar('Han Solo', $characters); ?></td>
 	<td><?php echo hasChar('Captain Han Solo', $characters); ?></td>
 	<td><?php echo hasChar('Hoth Rebel Scout', $characters); ?></td>
 	<td><?php echo hasChar('Hoth Rebel Soldier', $characters); ?></td>
@@ -11,7 +14,15 @@
 	<td><?php echo hasChar('Colonel Starck', $characters); ?></td>
 	<td><?php echo hasChar('Shoretrooper', $characters); ?></td>
 	<td><?php echo hasChar('Death Trooper', $characters); ?></td>
+	<td><?php echo hasChar('Snowtrooper', $characters); ?></td>
 	<td><?php echo hasChar('Grand Admiral Thrawn', $characters); ?></td>
+	<td><?php echo hasChar('Chirrut Îmwe', $characters); ?></td>
+	<td><?php echo hasChar('Baze Malbus', $characters); ?></td>
+	<td><?php echo hasChar('Asajj Ventress', $characters); ?></td>
+	<td><?php echo hasChar('Mother Talzin', $characters); ?></td>
+	<td><?php echo hasChar('Old Daka', $characters); ?></td>
+	<td><?php echo hasChar('Nightsister Acolyte', $characters); ?></td>
+	<td><?php echo hasChar('Nightsister Zombie', $characters); ?></td>
 	<td>&nbsp;</td>
 	<td><?php echo hasShip('Home One', $ships); ?></td>
 	<td><?php echo hasShip('Endurance', $ships); ?></td>
